c544696c90512179bccb90bb3d08d771e0a3615c
Fawaz Hussain Kazhungum Thottathil(UST <287917@ust.com>
2024-11-07 10:11:40 +0530

    added many functions in frontend

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import ResidenceGroup, Profile
 
 # Register your models here.
+admin.site.register(ResidenceGroup)
+admin.site.register(Profile)
--- frontend/src/components/RegisterForm.jsx
+++ frontend/src/components/RegisterForm.jsx
@@ -23,7 +23,7 @@ function RegisterForm({route,method}){
         e.preventDefault()
 
         try {
-            const res = await api.post(route, {username,password})
+            const res = await api.post(route, {username,password,phoneNumber,isAdmin,residenceCode})
             if (method === "login") {
                 localStorage.setItem(ACCESS_TOKEN, res.data.access)
                 localStorage.setItem(REFRESH_TOKEN, res.data.refresh)
